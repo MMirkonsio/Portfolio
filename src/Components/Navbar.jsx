d1906f893cca24357312bcf30b92329c2d3d2a9e
MMirkonsio <mirkovalencia90@gmail.com>
2024-04-05 18:53:45 -0300

    Notificaciones del correo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,40 +1,34 @@
 import Darkmode from "../Components/DarkMode";
 import { Github, Linkedin, Gmail } from "./Iconsvg";
-import { OverlayPanel } from "primereact/overlaypanel";
-import { useRef } from "react";
 import "../App.css";
-const Navbar = () => {
-  const op = useRef(null); // Ref para el OverlayPanel
+import { toast, Toaster } from "sonner";
 
+const Navbar = () => {
   const handleClickGmail = () => {
-    const email = "mirkovalencia90@gmail.com"; // Correo electrónico a copiar
-    navigator.clipboard.writeText(email); // Copiar el correo electrónico al Portapapeles
-    if (op.current) {
-      op.current.toggle(event); // Alternar la visibilidad del OverlayPanel
-      setTimeout(() => {
-        if (op.current) {
-          op.current.hide(); // Ocultar el OverlayPanel después de 5 segundos
-        }
-      }, 1000); // 5000 milisegundos = 5 segundos
-    }
+    const email = "mirkovalencia90@gmail.com";
+    navigator.clipboard.writeText(email);
+    toast.success("Correo copiado!", {
+      duration: 2000,
+      className: "my-classname",
+    });
   };
 
   return (
-    <div className="flex flex-row h-12 justify-between items-center w-full px-4 py-2 text-neutral-700 dark:text-gray-100 border-b-2 border-slate-400/10 bg-neutral-100  dark:bg-neutral-900 ">
+    <div className="flex flex-row h-12 justify-between items-center w-full px-4 py-2 text-neutral-700 dark:text-gray-100 border-b-2 border-slate-400/10 bg-neutral-100 dark:bg-neutral-900 mx-auto">
       <div className="flex flex-row justify-center items-center">
         <h1 className="text-2xl font-bold italic">@mirkovalencia</h1>
       </div>
-      <div className="flex gap-2">
+      <div className="flex gap-4 items-center">
         <Linkedin title="Linkedin" />
         <Github title="Github" />
-        {/* Icono de Gmail */}
-        <Gmail title="Gmail" onClick={handleClickGmail} />
-        {/* OverlayPanel */}
-        <OverlayPanel ref={op} className="gmail-overlay-panel">
-          <span className="p-1 text-sm rounded-lg dm-sans font-semibold italic text-green-700">
-            Correo copiado!
-          </span>
-        </OverlayPanel>
+        {/* Contenedor para los iconos y el botón de Gmail */}
+        <div className="flex items-center">
+          <button onClick={handleClickGmail}>
+            <Gmail />
+          </button>
+          {/* Componente Toastify para mostrar las notificaciones */}
+          <Toaster richColors position="top-right" />
+        </div>
       </div>
       <div className="flex items-center space-x-4 font-medium italic underline underline-offset-2">
         <Darkmode />
